c797de894e22c1147b7bd81debcd4bc02ae23e11
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-10-25 15:19:57 +0800

    change for conanos

--- build.py
+++ build.py
@@ -6,6 +6,8 @@ from bincrafters import build_template_default, build_shared
 import platform
 import os
 
+os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
+
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
