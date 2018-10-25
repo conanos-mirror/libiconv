17eb488a0933c24951e0846d4ff0444224dad78f
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-10-25 14:15:30 +0800

    init from https://github.com/bincrafters/conan-libiconv branch stable/1.15 commit ec6cc1e9f5136922636f09acc3733a2e6462fb3f

---
+++ build.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bincrafters import build_template_default, build_shared
+import platform
+import os
+
+if __name__ == "__main__":
+
+    builder = build_template_default.get_builder()
+
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if build_shared.get_os() == "Windows":
+            installers = ["cygwin_installer/2.9.0@bincrafters/stable"]
+            if os.getenv('MINGW_CONFIGURATIONS', ''):
+                installers.append("mingw_installer/1.0@conan/stable")
+            build_requires.update({"*": installers})
+    
+    builder.run()
