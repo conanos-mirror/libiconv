497a0d0dda78e210d4039f6af946bb6e1d18f5ca
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-11-09 16:04:10 +0800

    CI normalize

--- build.py
+++ build.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
-from bincrafters import build_template_default, build_shared
 import platform
 import os
+import re
+from cpt.packager import ConanMultiPackager
+from conanos.sdk.profile import filter
+
+__NAME__ = 'libiconv'
+
+if platform.system() == 'Windows':
+    os.environ['CONAN_VISUAL_VERSIONS'] = os.environ.get('CONAN_VISUAL_VERSIONS','15')
 
 os.environ['CONAN_USERNAME'] = os.environ.get('CONAN_USERNAME','conanos')
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
+    PATTERN = re.compile(r'conanio/(?P<compiler>gcc|clang)(?P<version>\d+)(-(?P<arch>\w+))?')
+    m = PATTERN.match(os.environ.get('CONAN_DOCKER_IMAGE',''))
+    docker_entry_script = ''
+    if m and os.path.exists('docker_entry_script.sh'):
+        compiler = m.group('compiler')
+        version  = m.group('version')
+        arch     = 'x86_64' if not m.group('arch') else m.group('arch')
+        docker_entry_script ='/bin/bash docker_entry_script.sh %s %s %s'%(compiler,version,arch)
+    
+        
+    builder = ConanMultiPackager(docker_entry_script=docker_entry_script)
+    builder.add_common_builds(pure_c=True)
 
-    builder = build_template_default.get_builder()
-
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        if build_shared.get_os() == "Windows":
-            installers = ["cygwin_installer/2.9.0@bincrafters/stable"]
-            if os.getenv('MINGW_CONFIGURATIONS', ''):
-                installers.append("mingw_installer/1.0@conan/stable")
-            build_requires.update({"*": installers})
+    filter(__NAME__,builder)
     
     builder.run()
